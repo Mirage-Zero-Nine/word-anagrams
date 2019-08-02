3123dc4892a226066757f06be2b49f8fd7f2f693
Null <borismirage00@gmail.com>
2019-08-02 13:45:08 -0700

    Add runme.sh

---
+++ runme.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+javac src/Entry.java src/GenerateKey.java src/DictionaryMap.java src/Anagram.java src/Reader.java
+
+# shellcheck disable=SC2164
+cd src
+
+java Entry edmo AaiLs OALle oplle
