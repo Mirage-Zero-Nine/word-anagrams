6f0361b66738be90671daadbc7e4858721fc1f73
Null <borismirage00@gmail.com>
2019-08-02 13:45:00 -0700

    Change file path

--- src/Entry.java
+++ src/Entry.java
@@ -15,14 +15,22 @@ public class Entry {
     public static void main(String[] args) {
         Anagram small;      // small dictionary
         Anagram large;      // large dictionary
+        System.out.println("Running test files in src/Test ...");
 
-        System.out.println("====== Small Dict ======");
-        small = new Anagram("src/Dictionary/tinyDictionary.txt");
-        small.getAnagrams("src/Test/Test1.txt");
+        System.out.println("====== Small Dictionary in src/Dictionary/tinyDictionary.txt ======");
+        small = new Anagram("Dictionary/tinyDictionary.txt");
+        small.getAnagrams("Test/Test1.txt");
 
-        System.out.println("====== Large Dict ======");
-        large = new Anagram("src/Dictionary/Dictionary.txt");
-        large.getAnagrams("src/Test/Test1.txt");
+        System.out.println("====== Large Dictionary in src/Dictionary/Dictionary.txt ======");
+        large = new Anagram("Dictionary/Dictionary.txt");
+        large.getAnagrams("Test/Test1.txt");
         large.getSingleWordAnagrams("poll");
+
+        if (args.length != 0) {
+            for (String arg : args) {
+                System.out.println("====== Finding anagrams of \"" + arg + "\" in src/Dictionary/Dictionary.txt ======");
+                large.getSingleWordAnagrams(arg);
+            }
+        }
     }
 }
