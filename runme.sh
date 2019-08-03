073402a7dc2274b76f0317e87b8899a8280d77fd
Null <borismirage00@gmail.com>
2019-08-02 20:50:32 -0700

    Add input for custom test case

--- runme.sh
+++ runme.sh
@@ -5,4 +5,16 @@ javac src/Entry.java src/GenerateKey.java src/DictionaryMap.java src/Anagram.jav
 # shellcheck disable=SC2164
 cd src
 
+echo "Running test case in src/Test"
+# add custom char set here with space
+java Entry
 java Entry edmo AaiLs OALle oplle
+
+while true; do
+  echo " "
+  echo "Please input char set here: "
+  read -r var
+  java Entry "$var"
+done
+
+exit 0
