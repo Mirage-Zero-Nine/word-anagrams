e05247affa7ee4b717af03bfeaa8112b3fa12809
Null <borismirage00@gmail.com>
2019-08-02 20:50:05 -0700

    Change entry call

--- src/Entry.java
+++ src/Entry.java
@@ -13,24 +13,27 @@ public class Entry {
      * @param args given arguments
      */
     public static void main(String[] args) {
-        Anagram small;      // small dictionary
-        Anagram large;      // large dictionary
-        System.out.println("Running test files in src/Test ...");
 
-        System.out.println("====== Small Dictionary in src/Dictionary/tinyDictionary.txt ======");
-        small = new Anagram("Dictionary/tinyDictionary.txt");
-        small.getAnagrams("Test/Test1.txt");
+        Anagram small = new Anagram("Dictionary/tinyDictionary.txt");;      // small dictionary
+        Anagram large = new Anagram("Dictionary/Dictionary.txt");;      // large dictionary
 
-        System.out.println("====== Large Dictionary in src/Dictionary/Dictionary.txt ======");
-        large = new Anagram("Dictionary/Dictionary.txt");
-        large.getAnagrams("Test/Test1.txt");
-        large.getSingleWordAnagrams("poll");
+        if (args.length > 0) {
 
-        if (args.length != 0) {
             for (String arg : args) {
                 System.out.println("====== Finding anagrams of \"" + arg + "\" in src/Dictionary/Dictionary.txt ======");
                 large.getSingleWordAnagrams(arg);
             }
+        } else {
+
+            System.out.println("Running test files in src/Test ...");
+
+            System.out.println("====== Small Dictionary in src/Dictionary/tinyDictionary.txt ======");
+            small.getAnagrams("Test/Test1.txt");
+
+            System.out.println("====== Large Dictionary in src/Dictionary/Dictionary.txt ======");
+            large = new Anagram("Dictionary/Dictionary.txt");
+            large.getAnagrams("Test/Test1.txt");
+            large.getSingleWordAnagrams("poll");
         }
     }
 }
